69c3c7b35f5ea7651494abf239bb041b36bde0ee
Aaryan Mishra <aaryan.mshra@gmail.com>
2026-03-22 20:59:22 +0530

    making it look better

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,4 +9,16 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+const work = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    description: z.string().optional(),
+    url: z.string().url().optional(),
+    tags: z.array(z.string()).optional(),
+    role: z.string().optional(),
+  }),
+});
+
+export const collections = { blog, work };
